d7d21884f595f8c3a34e587a5e258f7a4cc33d3e
Max Solodovnikov <maks_solod@list.ru>
2023-05-13 15:02:20 +0300

    комит

--- src/js/character.js
+++ src/js/character.js
@@ -1,13 +1,13 @@
 export default class Character {
-    constructor(name) {
-      if (typeof name !== 'string') {
-        throw new Error('Ошибка, имя должно быть строкой, а не числом или чем-то иным');
-      } else if (name.length < 2 || name.length > 10) {
-        throw new Error('Ошибка, длина имени должна быть от 2 до 10 символов');
-      } else {
-        this.name = name;
-      }
-      this.health = 100;
-      this.level = 1;
+  constructor(name) {
+    if (typeof name !== 'string') {
+      throw new Error('Ошибка, имя должно быть строкой, а не числом или чем-то иным');
+    } else if (name.length < 2 || name.length > 10) {
+      throw new Error('Ошибка, длина имени должна быть от 2 до 10 символов');
+    } else {
+      this.name = name;
     }
-  }
+    this.health = 100;
+    this.level = 1;
+   }
+}
